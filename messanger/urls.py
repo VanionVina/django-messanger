1eb4a0533bdc06831b2c9ff2a91eebe3fa9872a5
def <def@debian.com>
2021-01-31 01:28:21 +0500

    Add base html

--- messanger/urls.py
+++ messanger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ChatRoom
+from .views import ChatRoom, BaseView
 
 app_name = 'messanger'
 urlpatterns = [
-    path('chat-room', ChatRoom.as_view(), name='chat_room')
+    path('chat-room/', ChatRoom.as_view(), name='chat_room'),
+    path('base/', BaseView.as_view(), name='base')
 ]
--- messanger/views.py
+++ messanger/views.py
@@ -3,6 +3,9 @@ from django.views.generic import View
 
 
 class ChatRoom(View):
-
     def get(self, request):
         return render(request, 'chat_room.html')
+
+class BaseView(View):
+    def get(self, request):
+        return render(request, 'base.html')
